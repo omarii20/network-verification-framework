68399de2ab8aa75b710273acad6f8dd6aed4a550
Omarii_Mohamed <mohamedomarii44301@gmail.com>
2026-08-20 20:17:00 +0300

    test: add disconnect reconnect coverage and client factory fixture

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -5,11 +5,31 @@ HOST = "127.0.0.1"
 PORT = 8080
 
 
-@pytest.fixture
-def tcp_client():
+def create_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+    return client
+
+
+@pytest.fixture
+def tcp_client():
+    client = create_client()
 
     yield client
 
     client.close()
+
+
+@pytest.fixture
+def tcp_client_factory():
+    clients = []
+
+    def create():
+        client = create_client()
+        clients.append(client)
+        return client
+
+    yield create
+
+    for client in clients:
+        client.close()
